f25cf306befaace41af66a8d3e19b4bdc9816757
fkefszef <vqev22@gmail.com>
2025-02-21 13:54:17 +0100

    2.11

--- part2b/src/App.jsx
+++ part2b/src/App.jsx
@@ -1,58 +1,81 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Search from './components/Filter'
 import Person from './components/Person'
 import Show from './components/Show'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([])
+
   const [newName, setNewName] = useState('')
-
   const [newNumber, setNewNumber] = useState('')
-
   const [search, setSearch] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+      .catch(error => {
+        console.error('Error fetching data: ', error)
+      })
+  }, [])
 
   const addName = (event) => {
     event.preventDefault()
-    
-    if(persons.some(person => person.name === newName)){
+
+    if (persons.some(person => person.name === newName)) {
       alert(`${newName} is already added to phonebook`)
       return
     }
 
-    const newPerson = {name: newName, number: newNumber}
-    setPersons(persons.concat(newPerson))
-    setNewName('')
-    setNewNumber('');
-  }
-  
-  const handleNameChange = (event) => {
-    setNewName(event.target.value)  
+    const newPerson = { name: newName, number: newNumber }
+
+    // Send the new person data to the server (post request)
+    axios
+      .post('http://localhost:3001/persons', newPerson)
+      .then(response => {
+        setPersons(persons.concat(response.data))
+        setNewName('')
+        setNewNumber('')
+      })
+      .catch(error => {
+        console.error('Error adding new person: ', error)
+      })
   }
 
-  const handleNumberChange = (event) => {      
-    setNewNumber(event.target.value)  
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   const handleSearchChange = (event) => {
     setSearch(event.target.value)
   }
 
-  const filtered = persons.filter(person => person.name.toLowerCase().includes(search.toLowerCase()))
+  const filtered = persons.filter(person =>
+    person.name.toLowerCase().includes(search.toLowerCase())
+  )
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <Search search={search} handleSearchChange={handleSearchChange}></Search>
+      <Search search={search} handleSearchChange={handleSearchChange} />
       <h2>add a new</h2>
-      <Person addName={addName} newName={newName} handleNameChange={handleNameChange} newNumber={newNumber} handleNumberChange={handleNumberChange}></Person>
+      <Person
+        addName={addName}
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
+      />
       <h2>Numbers</h2>
-      <Show filtered={filtered}></Show>
-      </div>
+      <Show filtered={filtered} />
+    </div>
   )
 }
 
